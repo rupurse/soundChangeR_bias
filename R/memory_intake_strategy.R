50fd890c0bc272704c0d70b8af7ef22d6c3244e4
Ruaridh Purse <41395563+rupurse@users.noreply.github.com>
2025-10-01 12:00:10 -0400

    Update memory_intake_strategy.R

--- R/memory_intake_strategy.R
+++ R/memory_intake_strategy.R
@@ -1,4 +1,4 @@
-memory_intake_strategy <- function(strategy, exemplar, features, phoneme, agent, params, bias) {
+memory_intake_strategy <- function(strategy, exemplar, features, phoneme, agent, params, bias=NULL) {
   
   methodReg <- get_method_register()
   methodReg[params[["featureExtractionMethod"]], memoryIntakeStrategy][[1]][[strategy]](exemplar, features, phoneme, agent, params, bias)
