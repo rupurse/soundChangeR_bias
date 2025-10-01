5756f9f70e39dc05d1b630f2b251c4773385d7e5
Ruaridh Purse <41395563+rupurse@users.noreply.github.com>
2025-10-01 11:50:41 -0400

    Added eval bias options to compute_mahal_distance.R

--- R/compute_mahal_distance.R
+++ R/compute_mahal_distance.R
@@ -1,11 +1,18 @@
 compute_mahal_distance <- function(agent, features, phoneme, params) {
   
   if (!params[["useFlexiblePhonology"]]) {
-    stats::mahalanobis(features,
-                       base::apply(base::as.matrix(agent$features)[agent$memory$valid == TRUE & 
+    if (params[["useEvalBias"]]) {
+      stored <- base::as.matrix(agent$features)[agent$memory$valid == TRUE & agent$memory$phoneme == phoneme, , drop = FALSE]
+      ghost <- cbind(stored[,1], stored[,2] + (params[["evalBiasScale"]] * bias)
+      stats::mahalanobis(features,
+                          base::apply(ghost, 2, base::mean),
+                          stats::cov(ghost), tol = 1e-100)
+    } else {
+      stats::mahalanobis(features,
+                         base::apply(base::as.matrix(agent$features)[agent$memory$valid == TRUE & 
                                                                      agent$memory$phoneme == phoneme, , drop = FALSE], 2, base::mean),
-                       stats::cov(base::as.matrix(agent$features)[agent$memory$valid == TRUE &
-                                                                    agent$memory$phoneme == phoneme, , drop = FALSE]), tol = 1e-100)
+                         stats::cov(base::as.matrix(agent$features)[agent$memory$valid == TRUE &
+                                                                     agent$memory$phoneme == phoneme, , drop = FALSE]), tol = 1e-100)
 
   } else {
     GMM <- get_cache_value(agent, "GMM")
